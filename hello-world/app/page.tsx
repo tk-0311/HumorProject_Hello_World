85a8887033a3f1ce95277486e1c6e9b98a85f445
tk-0311 <113018868+tk-0311@users.noreply.github.com>
2026-09-21 16:59:48 -0400

    supabase was connected table created using dummy roster of names and emails

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+import Home from './index'
 
-export default function Home() {
+type RosterMember = {
+  id: number;
+  name: string;
+  email_prefix: string;
+  email_domain: string;
+};
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: roster } = await supabase.from('roster').select()
+  console.log('roster', roster)
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+    <Home/>
+    <table className="w-full border-collapse border border-gray-300">
+    <thead>
+      <tr className="bg-gray-700">
+        <th className="border border-gray-300 px-4 py-2 text-left">
+          Name
+        </th>
+        <th className="border border-gray-300 px-4 py-2 text-left">
+          Email
+        </th>
+      </tr>
+    </thead>
+      
+    <tbody>
+      {roster?.map((member: RosterMember) => (
+        <tr key={member.id}>
+          <td className="border border-gray-300 px-4 py-2">
+            {member.name}
+          </td>
+          <td className="border border-gray-300 px-4 py-2">
+            {member.email_prefix}@{member.email_domain}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+    </table>
+    </>
   )
 }
